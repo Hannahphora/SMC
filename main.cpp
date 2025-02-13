d2f4bca5146a29a83d1a136649e2a2ab1d643e65
Harrison Plowes <s240881@students.aie.edu.au>
2025-02-13 16:56:29 +1100

    commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using std::string;
 
 int main(int argc, char** argv) {
 
-	
+	string x;
+	std::fstream file("main.cpp");
 
+	while (std::getline(file, x)) {
+		std::cout << x << '\n';
+	}
+
+	file.close();
 	return 0;
 }
